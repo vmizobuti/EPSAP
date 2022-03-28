f8cb3d710977d92ec18f75369e7b859af387d142
Vinicius Mizobuti <vinicius@superlimao.com.br>
2022-03-28 11:56:59 -0300

    added comments to DXFtoBoundaries function

--- epsap.py
+++ epsap.py
@@ -1,4 +1,4 @@
-# Implements the evolutionary program for the space allocation problem 
+# Implements the evolutionary program for space allocation problem 
 # proposed by Rodrigues, E. et. al (2013).
 # 
 # Author: Vinicius Mizobuti
@@ -21,39 +21,58 @@ def DXFtoBoundaries(filepath):
     Imports a DXF file and transforms its polylines to boundaries according to their layers. The DXF file must contain at least one closed polyline in the layer 'building' and can contain multiple closed polylines on the layer 'adjacent'. The adjacent buildings can't have overlaps with the building boundary. 
     """
     # Reads and parses the DXF file.
-
     dxf_file = ezdxf.readfile(filepath)
-
     building = dxf_file.query('LWPOLYLINE[layer=="building"]')
     adjacent = dxf_file.query('LWPOLYLINE[layer=="adjacent"]')
 
+    # Creates the dictionary for polyline points and the list for output contents
     building_points = {}
     adjacent_points = {}
-    dxf_contents = []
+    dxf_contents = {}
 
+    # Creates the COMPAS Points for the building boundary
     for polyline in building.entities:
+        # Gets the points from the DXF Polyline
         polyline_points = polyline.get_points()
         building_points[polyline] = []
+        
+        # Transforms the DXF Coordinates into COMPAS Points
         for point in polyline_points:
             compas_point = Point(round(point[0], 1), round(point[1], 1), 0.0)
             building_points[polyline].append(compas_point)
 
+    # Creates the COMPAS Points for the adjacent buildings
     for polyline in adjacent.entities:
+        # Gets the points from the DXF Polyline
         polyline_points = polyline.get_points()
         adjacent_points[polyline] = []
+        
+        # Transforms the DXF Coordinates into COMPAS Points
         for point in polyline_points:
             compas_point = Point(round(point[0], 1), round(point[1], 1), 0.0)
             adjacent_points[polyline].append(compas_point)
 
+    # Creates the COMPAS Polylines for the building boundary
     for polyline in building_points.keys():
+        # Duplicates the first point to ensure that the Polyline is closed
         building_points[polyline].append(building_points[polyline][0])
+        
+        # Creates the COMPAS Polyline given a list of points
         building_boundary = Polyline(building_points[polyline])
-        dxf_contents.append(building_boundary)
+        
+        # Adds the polyline to the "building" key in the contents dictionary
+        dxf_contents["building"] = building_boundary
     
+    # Creates the COMPAS Polylines for the adjacent buildings
     for polyline in adjacent_points.keys():
+        # Duplicates the first point to ensure that the Polyline is closed
         adjacent_points[polyline].append(adjacent_points[polyline][0])
+        
+        # Creates the COMPAS Polyline given a list of points
         adjacent_boundary = Polyline(adjacent_points[polyline])
-        dxf_contents.append(adjacent_boundary)
+        
+        # Adds the polyline to the "adjacent" key in the contents dictionary
+        dxf_contents["adjacent"] = adjacent_boundary
 
     return dxf_contents
 
